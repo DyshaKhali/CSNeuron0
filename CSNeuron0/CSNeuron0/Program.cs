2820b3ecd9d14ed4ca9e3d1c6fbe9df8c1fb282f
DYSHA <1337mandarinka@mail.ru>
2022-04-23 12:50:21 +0500

    CSNeuron0
    Converter km to miles

---
+++ CSNeuron0/CSNeuron0/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace CSNeuron0
+{
+    class Program
+    {
+        public class Neuron
+        {
+            private decimal weight = 0.5m;
+
+            public decimal LastError { get; private set; }
+
+            public decimal Smoothing { get; set; } = 0.00001m;
+            public decimal ProcessInputData(decimal input)
+            {
+                return input * weight;
+            }
+
+            public decimal RestoreInputData(decimal output)
+            {
+                return output / weight;
+            }
+
+            public void Train(decimal input, decimal expectedResult)
+            {
+                var actualResult = input * weight;
+                LastError = expectedResult - actualResult;
+                var correction = (LastError / actualResult) * Smoothing;
+                weight += correction;
+            }
+        }
+        static void Main(string[] args)
+        {
+            decimal km = 100;
+            decimal miles = 62.1371m;
+
+            Neuron neuron = new Neuron();
+
+            int i = 0;
+            do
+            {
+                i++;
+                neuron.Train(km, miles);
+                Console.WriteLine($" Итерация: {i}\t Ошибка: \t{neuron.LastError}");
+
+            } while (neuron.LastError > neuron.Smoothing || neuron.LastError < -neuron.Smoothing);
+
+            Console.WriteLine($" {neuron.ProcessInputData(100)} миль в {100} км");
+        }
+    }
+}
